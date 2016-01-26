09f7ecaaab62784c5565dbad153dc47cc3742adc
Evan Kuchar <evan.kuchar@gmail.com>
2016-01-26 07:42:09 -0800

    embetter

--- content.js
+++ content.js
@@ -1,3 +1,33 @@
+
+$(function() {
+  var elements = document.getElementsByTagName('*');
+
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    if (element.tagName === 'SCRIPT' || element.tagName === 'HEAD' || element.tagName === 'STYLE') continue;
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+      var node = element.childNodes[j];
+      if (node.nodeType === 3) {
+        var text = node.nodeValue;
+
+        var replacedText = text.
+          replace(/good/g, 'resonant').
+          replace(/mag.{0,1}net/g, 'ʇǝubɐɯ').
+          replace(/Mag.{0,1}net/g, 'ʇǝubɐW').
+          replace(/(\(not so\!\) fast)|fast/gi, '(not so!) fast').
+          replace(/per.{0,1}son/gi, 'supplicant').
+          replace(/Per.{0,1}son/gi, 'Supplicant');
+
+        if (replacedText !== text) {
+
+          element.replaceChild(document.createTextNode(replacedText), node);
+        }
+      }
+    }
+  }
+});
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.message === "clicked_browser_action") {
@@ -8,6 +38,9 @@ chrome.runtime.onMessage.addListener(
 
 function quasify() {
   var $div = $('<div>');
+  $div.css({
+    textAlign: 'center',
+  })
   $div.css({
     height: window.innerHeight,
     width: window.innerWidth,
@@ -25,36 +58,69 @@ function quasify() {
   $qvaz.css({
     margin: '0 auto',
     width: '577px',
+    fontSize: '12px',
+    lineHeight: '13px',
   });
 
   $div.html($qvaz);
 
-  function printQuaz() {
-    printRows($qvaz, qvaz, 40, 100);
+  function printQuaz(cb) {
+    printRows($qvaz, qvaz, 40, 100, cb);
   }
 
   $('body').append($div.hide());
 
-  $('body > div').fadeOut(3000)
-  $div.fadeIn(2000, printQuaz);
+  $beforeDivs = $('body > div:visible');
+  $beforeDivs.fadeOut(3000)
+  $div.fadeIn(2000, function() {
+    printQuaz(printQuazHowl);
+  });
 
   pink(100, 10000);
 
-  setTimeout(function() {
+  function printQuazHowl() {
     $qvaz.fadeOut(2000, function() {
       $qvaz.remove()
       $quazHowl = $('<div>');
       $quazHowl.css({
+        textAlign: 'left',
         whiteSpace: 'pre',
         fontSize: '25px',
         width: '940px',
         margin: '0 auto',
         paddingTop: '100px',
-      })
+      });
       $div.html($quazHowl);
-      printRows($quazHowl, quazHowl, 70, 2600);
+
+      function _printQuazHowl(rows, cb) {
+        var $row = $('<div>');
+        $quazHowl.append($row);
+        $row.css({
+          fontFamily: 'monospace'
+        });
+        var row = rows.shift();
+        if (row) {
+          var _cb = rows.length > 0 ? null : cb
+          printLine($row, row, 70, function() {
+            _printQuazHowl(rows, cb);
+            if (_cb) _cb();
+          });
+        }
+      }
+
+      _printQuazHowl(quazHowl, function() {
+        $quazHowl.fadeOut(2000, function() {
+          $quazHowl.remove()
+          showAskQuaz($div, function() {
+            $div.fadeOut(2000, function() {
+              $beforeDivs.fadeIn(3000)
+            });
+          });
+        });
+      });
+
     });
-  }, 12000);
+  }
 }
 
 function printRows($container, arr, letterSpeed, rowSpeed, cb) {
@@ -67,24 +133,27 @@ function printRows($container, arr, letterSpeed, rowSpeed, cb) {
     })
     $container.append($rowDiv);
     setTimeout(function() {
-      var letterSpeedAcc = 0;
-      for (var i = 0; i < row.length ; i++) {
-        (function(_i) {
-          letterSpeedAcc += letterSpeed * Math.random() * 2;
-          setTimeout(function() {
-            var ltr = row.slice(_i, _i + 1);
-            $rowDiv.append(ltr);
-
-            var isLastLetter = _i === (row.length - 1);
-            var isLastRow = rowIndex === (arr.length - 1);
-            if (isLastLetter && isLastRow) {
-              if (cb) cb();
-            }
-          }, letterSpeedAcc);
-        })(i)
-      }
+      var _cb = rowIndex === (arr.length - 1) ? cb : null;
+      printLine($rowDiv, row, letterSpeed, _cb);
     }, rowSpeedAcc);
   });
+}
+
+function printLine($rowDiv, row, letterSpeed, cb) {
+  var letterSpeedAcc = 0;
+  for (var i = 0; i < row.length ; i++) {
+    (function(_i) {
+      letterSpeedAcc += letterSpeed * Math.random() * 2;
+      setTimeout(function() {
+        var ltr = row.slice(_i, _i + 1);
+        $rowDiv.append(ltr);
+        var isLastLetter = _i === (row.length - 1);
+        if (isLastLetter && cb) {
+          cb();
+        }
+      }, letterSpeedAcc);
+    })(i)
+  }
 }
 
 
@@ -201,3 +270,62 @@ var quazHowl = [
   "    Qu'az whose breast is a herbivore dynamo! Kwaa'asss whose",
   "    ears are a tuning fork!",
 ];
+
+
+function showAskQuaz($div, cb) {
+  var $askQuas = $('<div>');
+  var askQuas = '<div>SUPPLICANT!<div><div>YOU MAY NOW SUPPLICATE!</div><div>WHAT IS IT THAT YOU WISH?<div style="font-size: 70px">SPEAK NOW.</div>';
+
+  $askQuas.css({
+    whiteSpace: 'pre',
+    fontSize: '50px',
+    lineHeight: '60px',
+    width: '940px',
+    margin: '0 auto',
+    paddingTop: '100px',
+  });
+
+  $click = $('<div style="cursor: pointer; margin-top: 100px">SUPPLICATE</div>').hide()
+  $click.click(function() {
+    $askQuas.fadeOut(function() {
+      $askQuas.remove()
+      showQuazReply($div, cb);
+    });
+  });
+
+  setTimeout(function() {
+    $click.fadeIn();
+  }, 5000);
+
+  $askQuas.html(askQuas);
+  $askQuas.append($click);
+
+  $div.html($askQuas);
+
+}
+
+function showQuazReply($div, cb) {
+  var quazReplies = [
+    'Outlook resonant',
+    'Outlook not so good',
+    'Concentrate and ask again',
+    'Don\'t count on it',
+    'Outlook not so resonant',
+    'My Reply is no',
+  ];
+  var quazReply = quazReplies[Math.floor(Math.random() * (quazReplies.length - 1))];
+
+  var $quazReply = $('<div>' + quazReply + '</div>').hide();
+  $quazReply.css({
+    whiteSpace: 'pre',
+    fontSize: '50px',
+    lineHeight: '60px',
+    width: '940px',
+    margin: '0 auto',
+    paddingTop: '100px',
+  });
+
+  $div.html($quazReply.fadeIn(2000, function() {
+    setTimeout(cb, 2000);
+  }));
+}
